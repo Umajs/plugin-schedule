9626ddcca5d99bf35cd65b2429e46fb13b42a101
hengg <wangliang826@hotmail.com>
2022-03-16 15:59:00 +0800

    chore:ignore /lib

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Uma, TPlugin } from "@umajs/core";
-import { scheduleMap, AbstractSchedule, umaTask } from "./schedule";
-declare const _default: (uma: Uma) => TPlugin;
-export default _default;
-export { AbstractSchedule, scheduleMap, umaTask };
